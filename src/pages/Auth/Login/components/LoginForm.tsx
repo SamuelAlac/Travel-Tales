22605a3240573791743e7acd1639e20aa9bf3816
SamuelAlac <samuelalac21@gmail.com>
2025-09-17 16:52:23 +0800

    add: registration

--- src/pages/Auth/Login/components/LoginForm.tsx
+++ src/pages/Auth/Login/components/LoginForm.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export const LoginForm = () => {
   return (
     <form action="" className="min-h-120 p-5 md:rounded-4xl flex flex-col justify-center bg-neutral-50/30">
-        <legend className="text-green-700 text-5xl text-center mb-7">Travel Tales</legend>
+        <legend className="text-green-700 text-4xl text-center mb-7">Welcome Back</legend>
             <label className="text-black mb-2">Email
             <input className="input text-neutral-50 rounded-4xl" type="email" required placeholder="name@example.com"
             title="Email should like like name@example.com" />
@@ -14,11 +14,11 @@ export const LoginForm = () => {
 
             <label className="text-black my-3">Password
             <input type="password" className="input text-neutral-50 rounded-4xl" required placeholder="Password"
-            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter" />
+            title="Password Must be more than 8 characters, including number, lowercase letter, uppercase letter" />
             </label>
         <Link to='/' className="text-blue-600 text-end mb-5 hover:underline">Forgot password?</Link>
-        <button className="btn btn-soft rounded-4xl mb-5">Login</button>
-        <p className="text-black text-center mb-5">Or Login with</p>
+        <button className="btn btn-soft rounded-4xl mb-5">Sign In</button>
+        <p className="text-black text-center mb-5">Or Sign In with</p>
         <div className="text-black flex justify-center gap-5 mb-2 md:mb-8">
             <FaFacebook size={35} role="button"/>
             <FaGoogle size={35} role="button"/>
--- src/pages/Auth/Register/components/RegisterForm.tsx
+++ src/pages/Auth/Register/components/RegisterForm.tsx
@@ -6,27 +6,37 @@ import { Link } from "react-router-dom";
 export const RegisterForm = () => {
   return (
     <form action="" className="min-h-120 p-5 md:rounded-4xl flex flex-col justify-center bg-neutral-50/30">
-        <legend className="text-green-700 text-5xl text-center mb-7">Travel Tales</legend>
-            <label className="text-black mb-2">Email
+        <legend className="text-green-700 text-4xl text-center mb-7">Create Account</legend>
+
+            <label className="text-black mb-2 text-sm">Username
+            <input type="text" className="input text-neutral-50 rounded-4xl" required placeholder="Username" 
+            title="Username should only have letters and numbers" />
+            </label>
+
+            <label className="text-black mb-2 text-sm">Email
             <input className="input text-neutral-50 rounded-4xl" type="email" required placeholder="name@example.com"
             title="Email should like like name@example.com" />
             </label>
 
-            <label className="text-black my-3">Password
-            <input type="password" className="input text-neutral-50 rounded-4xl" required placeholder="Password"
-            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter" />
+            <label className="text-black my-3 text-sm">Password
+            <input type="password" className="input text-neutral-50 rounded-4xl" required placeholder="Confirm Password"
+            title="Password Must be more than 8 characters, including number, lowercase letter, uppercase letter" />
             </label>
-        <Link to='/' className="text-blue-600 text-end mb-5 hover:underline">Forgot password?</Link>
-        <button className="btn btn-soft rounded-4xl mb-5">Login</button>
-        <p className="text-black text-center mb-5">Or Login with</p>
+
+            <label className="text-black my-3 text-sm">Confirm Password
+            <input type="password" className="input text-neutral-50 rounded-4xl" required placeholder="Password"
+            title="Password Must be more than 8 characters, including number, lowercase letter, uppercase letter" />
+            </label>
+        <button className="btn btn-soft rounded-4xl my-5">Sign Up</button>
+        <p className="text-black text-center mb-5">Or Sign Up with</p>
         <div className="text-black flex justify-center gap-5 mb-2 md:mb-8">
             <FaFacebook size={35} role="button"/>
             <FaGoogle size={35} role="button"/>
             <FaGithub size={35} role="button"/>
         </div>
 
-        <p className="text-black text-center">Don't have an account?
-            <Link to='/' className="text-blue-600 underline ms-2">Sign Up</Link>
+        <p className="text-black text-center">Already have an account?
+            <Link to='/Login' className="text-blue-600 underline ms-2">Sign In</Link>
         </p>
     </form>
   )
